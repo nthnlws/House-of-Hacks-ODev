ea82a8c9c32e501f1f784153da8c7a177abf1d13
Mohamed <javidrahim2005@gmail.com>
2024-11-02 10:59:14 -0400

    fixed app function

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "House-of-Hacks-ODev",
+  base: "/House-of-Hacks-ODev/",
   plugins: [react()],
 })
